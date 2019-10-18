9fd12f10160d8658d8f743181d496f23d05e6db1
aniamirjanyan <54023925+aniamirjanyan@users.noreply.github.com>
2019-10-18 17:11:45 +0400

    Add files via upload

--- final1.py
+++ final1.py
@@ -1,39 +1,36 @@
 import subprocess
 from influxdb import InfluxDBClient
 from datetime import date
-import time
+import datetime
 
 
 def main():
+    i = 0
     while True:
-        a = subprocess.Popen(["ps -eo user,%mem,%cpu,start"], shell=True, stdout=subprocess.PIPE, encoding='utf-8')  
+        a = subprocess.Popen(["ps -eo user,%mem,%cpu,lstart"], shell=True, stdout=subprocess.PIPE, encoding='utf-8')
         output = a.stdout.readlines()
         if not output:
             break
-        print(output, end = "\n")
-        
+        print(output, end="\n")
+
         headers = [h for h in ' '.join(output[0].strip().split()).split() if h]
         raw_data = map(lambda s: s.strip().split(None, len(headers) - 1), output[1:])
         data = [dict(zip(headers, r)) for r in raw_data]
-        data1 = [{"measurement": "m",
-              "time": str(date.today())+"T"+i["STARTED"]+"Z",
-            "fields": i} for i in data]
-        
+        data1 = [{"measurement": "cpu_mem",
+                  "time": str(datetime.datetime.strptime(i["STARTED"], "%a %b %d %H:%M:%S %Y"))[:10] + "T" + str(
+                      datetime.datetime.strptime(i["STARTED"], "%a %b %d %H:%M:%S %Y"))[11:] + "Z",
+                  "fields": i} for i in data]
+
         user = 'root'
         password = 'root'
-        dbname = 'dbname'
-
+        dbname = 'db'
 
         client = InfluxDBClient('localhost', 8086, user, password, dbname)
         client.create_database(dbname)
 
         for j in data1:
             client.write_points([j])
-        
-        time.sleep(1)
-          
 
 
 if __name__ == '__main__':
     main()
-
